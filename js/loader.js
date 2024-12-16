a659cbfcfd89a7727ced2ab77ce5672355c08175
yushi4ka <yushdark2424@gmail.com>
2024-12-16 23:51:50 +0500

    Delete js directory

--- js/loader.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        document.querySelectorAll('.project-card').forEach(card => {
-            card.classList.add('loaded');
-        });
-    }, 250);
-});
